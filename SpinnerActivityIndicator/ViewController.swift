cd871077406a612a74c7079e9ee1f6962d58b2f3
Alexander Tkachenko <atkachenko@megogo.net>
2017-08-22 22:10:04 +0300

    Initial Commit

---
+++ SpinnerActivityIndicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpinnerActivityIndicator
+//
+//  Created by Alexander Tkachenko on 8/22/17.
+//  Copyright © 2017 megogo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
