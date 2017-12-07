b69b75ad4674b5d1cba11d7d9f99f78b18902d4c
Alex Tkachenko <tkach2004@gmail.com>
2017-12-08 00:57:45 +0200

    Fix project file error

--- SpinnerActivityIndicatorExample/MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-import PlaygroundSupport
-//@testable import SpinnerActivityIndicator
-
-var str = "Hello, playground"
-let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateInitialViewController()
-PlaygroundPage.current.liveView = vc
